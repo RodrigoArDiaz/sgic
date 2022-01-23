753cbabd84eecaed7fa0f3fee5db20f9f0e286e7
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-01-23 15:09:02 -0300

    feat: Carpeta Styles

---
+++ src/styles/EstilosFormularioIniciarSesion.js
@@ -0,0 +1,58 @@
+// Estilos
+const estiloPaper = {
+  height: "auto",
+  width: { xs: "100%", sm: "380px" },
+  margin: { xs: "0 auto", sm: "100px auto" },
+  boxShadow: { xs: 0, sm: 8 },
+};
+
+const estiloFormControl = {
+  width: "100%",
+};
+
+const estiloBoxForm = {
+  display: "flex",
+  alignItems: "flex-end",
+  width: "100%",
+  mt: { xs: "42px", md: "25px" },
+};
+
+const estiloButton = {
+  mt: "60px",
+};
+
+const estiloLink = {
+  mt: "15px",
+};
+
+const estiloHeader = {
+  backgroundColor: "primary.main",
+  color: "white",
+  py: "20px",
+  borderRadius: { xs: "none", md: "4px 4px 0 0" },
+  mb: "10px",
+  borderBottom: { xs: "0px", sm: "2px solid" },
+  borderColor: "secondary.light",
+  boxShadow: { xs: 4, sm: 0 },
+};
+
+const estiloContent = {
+  padding: "5px 40px 40px 40px ",
+};
+
+const estiloIconoUsuario = {
+  width: "50px",
+  height: "50px",
+  bgcolor: "#000",
+};
+
+export {
+  estiloPaper,
+  estiloFormControl,
+  estiloBoxForm,
+  estiloButton,
+  estiloLink,
+  estiloHeader,
+  estiloContent,
+  estiloIconoUsuario,
+};
---
+++ src/styles/EstilosFormularioRegistro.js
@@ -0,0 +1,45 @@
+/**
+ * Estilos.
+ */
+const estiloPaper = {
+  height: "auto",
+  width: { xs: "100%", sm: "490px" },
+  margin: { xs: "0 auto", sm: "20px auto" },
+  boxShadow: { xs: 0, sm: 8 },
+};
+
+const estiloFormControl = {
+  width: "100%",
+  mt: "25px",
+};
+
+const estiloButton = {
+  mt: "50px",
+};
+
+const estiloLink = {
+  mt: "30px",
+};
+
+const estiloHeader = {
+  backgroundColor: "primary.main",
+  color: "white",
+  py: "15px",
+  borderRadius: { xs: "none", md: "4px 4px 0 0" },
+  mb: "10px",
+  borderBottom: "2px solid",
+  borderColor: "secondary.light",
+};
+
+const estiloContent = {
+  padding: "5px 40px 40px 40px ",
+};
+
+export {
+  estiloPaper,
+  estiloFormControl,
+  estiloButton,
+  estiloLink,
+  estiloHeader,
+  estiloContent,
+};
