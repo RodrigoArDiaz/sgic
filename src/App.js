9de8d8a00747043b059140e1b8f185407699a1f8
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-01-23 20:25:19 -0300

    feat: Inicio y cierre de sesion

--- src/App.js
+++ src/App.js
@@ -1,82 +1,113 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import temaConfig from './temaConfig.js';
-import MiniDrawers from './components/MiniDrawers';
-import PaginaCatedras from './pages/PaginaCatedras.js';
-import PaginaDocentes from './pages/PaginaDocentes.js';
-import PaginaAlumnos from './pages/PaginaAlumnos.js';
-import PaginaInicioSesion from './pages/PaginaInicioSesion.js';
-import PaginaRegistrarse from './pages/PaginaRegistrarse.js';
-import PaginaRecuperarContrasenia from './pages/PaginaRecuperarContrasenia.js';
-
+import React from "react";
+import { ThemeProvider } from "@mui/material";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import temaConfig from "./temaConfig.js";
+import MiniDrawers from "./components/MiniDrawers";
+import PaginaCatedras from "./pages/PaginaCatedras.js";
+import PaginaDocentes from "./pages/PaginaDocentes.js";
+import PaginaAlumnos from "./pages/PaginaAlumnos.js";
+import PaginaInicioSesion from "./pages/PaginaInicioSesion.js";
+import PaginaRegistrarse from "./pages/PaginaRegistrarse.js";
+import PaginaRecuperarContrasenia from "./pages/PaginaRecuperarContrasenia.js";
+import { useSelector } from "react-redux";
 
 export default function App() {
-    return (
-        <> 
-            <Router>
-                
+  const login = useSelector((state) => state.login);
+  const { isAuth } = login;
 
-                <ThemeProvider theme={temaConfig}>
-                    <Routes>
+  return (
+    <>
+      <Router>
+        <ThemeProvider theme={temaConfig}>
+          <Routes>
+            {/* Rutas publicas */}
+            <Route
+              path="/"
+              element={
+                !isAuth ? <PaginaInicioSesion /> : <Navigate to="/inicio" />
+              }
+            />
 
-                        <Route
-                            path="/"
-                            element={<PaginaInicioSesion />}
-                        />
+            <Route path="/acceso_alumno" element={<PaginaInicioSesion />} />
 
-                        <Route
-                            path="/iniciar_sesion_alumno"
-                            element={<PaginaInicioSesion/>}
-                        />
+            <Route
+              path="/acceso_docente"
+              element={<PaginaInicioSesion mostrarRegistrarse={false} />}
+            />
 
-                        <Route
-                            path="/iniciar_sesion_super"
-                            element={<PaginaInicioSesion mostrarRegistrarse={false}/>}
-                        />
+            <Route
+              path="/acceso_superadministrador"
+              element={<PaginaInicioSesion mostrarRegistrarse={false} />}
+            />
 
-                        <Route
-                            path="/iniciar_sesion_docente"
-                            element={<PaginaInicioSesion mostrarRegistrarse={false}/>}
-                        />
+            <Route path="/registrarse" element={<PaginaRegistrarse />} />
 
-                        
+            <Route
+              path="/recuperar_contrasenia"
+              element={<PaginaRecuperarContrasenia />}
+            />
 
-                        <Route
-                            path="/registrarse"
-                            element={<PaginaRegistrarse/>}
-                        />
+            {/* Rutas privada: rol alumno*/}
+            <Route
+              path="/inicio/*"
+              element={
+                isAuth ? (
+                  <MiniDrawers></MiniDrawers>
+                ) : (
+                  <Navigate to="/acceso_alumno" />
+                )
+              }
+            >
+              {/* <Route path="catedras" element={<PaginaCatedras />} />
 
-                        <Route
-                            path="/recuperar_contrasenia"
-                            element={<PaginaRecuperarContrasenia/>}
-                        />
+              <Route path="docentes" element={<PaginaDocentes />} />
 
-                        <Route
-                            path="/inicio/*"
-                            element={<MiniDrawers></MiniDrawers>}
-                        >
-                                <Route
-                                path="catedras"
-                                element={<PaginaCatedras/>}
-                                />
-                                
-                                <Route
-                                    path="docentes"
-                                    element={<PaginaDocentes/>}
-                                />
-                            
-                                <Route
-                                    path="alumnos"
-                                    element={<PaginaAlumnos/>}
-                                />  
-                         </Route>
+              <Route path="alumnos" element={<PaginaAlumnos />} /> */}
+            </Route>
 
-                    </Routes>
-                </ThemeProvider> 
-            </Router>
-        </>
-    ); 
+            {/* Rutas privada: rol docente*/}
+            <Route
+              path="/inicio_docente/*"
+              element={
+                isAuth ? (
+                  <MiniDrawers></MiniDrawers>
+                ) : (
+                  <Navigate to="/acceso_docente" />
+                )
+              }
+            >
+              {/* <Route path="catedras" element={<PaginaCatedras />} />
+
+              <Route path="docentes" element={<PaginaDocentes />} />
+
+              <Route path="alumnos" element={<PaginaAlumnos />} /> */}
+            </Route>
+
+            {/* Rutas privada: rol supervisor*/}
+            <Route
+              path="/inicio_superadministrador/*"
+              element={
+                isAuth ? (
+                  <MiniDrawers></MiniDrawers>
+                ) : (
+                  <Navigate to="/acceso_superadministrador" />
+                )
+              }
+            >
+              <Route path="catedras" element={<PaginaCatedras />} />
+
+              <Route path="docentes" element={<PaginaDocentes />} />
+
+              <Route path="alumnos" element={<PaginaAlumnos />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </Router>
+    </>
+  );
 }
-
-
--- src/components/CerrarSesion.js
+++ src/components/CerrarSesion.js
@@ -1,62 +1,81 @@
-import React from 'react';
-import { Button } from '@mui/material';
-import LogoutIcon from '@mui/icons-material/Logout';
-import { useModal } from '../hooks/useModal';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React from "react";
+import { Button } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useModal } from "../hooks/useModal";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useDispatch } from "react-redux";
+import { userReset } from "../store/slices/userSlice";
+import { loginReset } from "../store/slices/loginSlice";
+import { logoutUsuario } from "../api/sgicApi";
+import { useSelector } from "react-redux";
 
-export default function CerrarSesion({responsive}) {
-    const [isOpen, handleOpen, handleClose] = useModal(false);
+export default function CerrarSesion({ responsive }) {
+  const [isOpen, handleOpen, handleClose] = useModal(false);
 
-    return (
-        <>
-            
+  const dispatch = useDispatch();
 
-            {responsive ?
-                <Button 
-                    size="large"
-                    variant="outlined"
-                    color="secondary"
-                    endIcon={<LogoutIcon/>}
-                    elevation={12}
-                    onClick={handleOpen}
-                    >
-                        Cerrar Sesion
-                </Button>
-                :
-                <Button 
-                    sx={{display: {xs: 'none', sm:'flex'}}}
-                    variant= "contained"
-                    color="primary"
-                    disableElevation      
-                    endIcon={<LogoutIcon/>}
-                    onClick={handleOpen}
-                >    
-                    Cerrar Sesion
-                </Button>
-            }
+  const { token } = useSelector((state) => state.login);
 
-            {/* Ventana modal */}
-            <Dialog 
-                open={isOpen} 
-                onClose={handleClose}
-                maxWidth="xs"
-                fullWidth
-            >
-                <DialogTitle>Cerrar Sesion</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        ¿Seguro que desea cerrar sesión?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant='contained' onClick={handleClose}>Aceptar</Button>
-                    <Button variant='outlined'  color="secondary" onClick={handleClose}>Cancelar</Button>
-                </DialogActions>
-            </Dialog> 
+  const handleLogout = async (e) => {
+    handleClose();
+    try {
+      const res = await logoutUsuario(token);
+      dispatch(userReset());
+    } catch (error) {
+      console.log(error);
+    }
+    //Resetea los datos de login(authenticacion) y de usuario
+    dispatch(loginReset());
+    dispatch(userReset());
+  };
+
+  return (
+    <>
+      {responsive ? (
+        <Button
+          size="large"
+          variant="outlined"
+          color="secondary"
+          endIcon={<LogoutIcon />}
+          elevation={12}
+          onClick={handleOpen}
+        >
+          Cerrar Sesion
+        </Button>
+      ) : (
+        <Button
+          sx={{ display: { xs: "none", sm: "flex" } }}
+          variant="contained"
+          color="primary"
+          disableElevation
+          endIcon={<LogoutIcon />}
+          onClick={handleOpen}
+        >
+          Cerrar Sesion
+        </Button>
+      )}
+
+      {/* Ventana modal */}
+      <Dialog open={isOpen} onClose={handleClose} maxWidth="xs" fullWidth>
+        <DialogTitle>Cerrar Sesion</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ¿Seguro que desea cerrar sesión?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={handleLogout}>
+            Aceptar
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
-    )
+  );
 }
