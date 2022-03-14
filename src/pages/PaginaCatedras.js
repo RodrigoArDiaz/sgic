fb5b584824124c8b032be78c39b30a926cf8b273
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-03-14 16:40:36 -0300

    style: Cambio de estilo en gestion catedras (super)

--- src/pages/PaginaCatedras.js
+++ src/pages/PaginaCatedras.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import { Grid } from '@mui/material'
-import BreadCrumbs from '../components/BreadCrumbs'
-import BuscarCatedras from '../components/Catedras/BuscarCatedras';
-import CatedrasContenedor from '../components/Catedras/CatedrasContenedor';
+import React, { useEffect } from "react";
+import { Grid } from "@mui/material";
+import BreadCrumbs from "../components/BreadCrumbs";
+import CatedrasContenedor from "../components/Catedras/CatedrasContenedor";
 
-import { ThemeProvider } from '@mui/material';
-import temaConfig from '../temaConfig.js';
-
+import { useDispatch } from "react-redux";
+import { actualizarTitulo } from "../store/slices/menuSlice";
+import { estilosBreadCrumbs } from "../styles/EstilosPaginas";
 
 export default function PaginaCatedras() {
-    return (
+  //Para el uso de funciones de los state de redux
+  const dispatch = useDispatch();
 
+  //Actualiza el titulo al montar la pagina
+  useEffect(() => {
+    dispatch(actualizarTitulo("Superadministrador"));
+  }, []);
 
-        
-         <ThemeProvider theme={temaConfig}>
-        <Grid
-            container
-            sx={{ml: {xs: "0", sm: "20px"} ,mt: "80px"}}
-        >
-             <Grid item xs={12}>
-                {/* <BreadCrumbs/> */}
-            </Grid>
+  //Actualiza el titulo al desmontar la pagina
+  useEffect(() => {
+    return () => {
+      dispatch(actualizarTitulo(""));
+    };
+  }, []);
 
-           
-
-            <Grid item xs={12}>
-                 <CatedrasContenedor/>
-            </Grid>
+  return (
+    <Grid container rowSpacing={3}>
+      <Grid item xs={12}>
+        <Grid container sx={estilosBreadCrumbs}>
+          <BreadCrumbs />
         </Grid>
-        </ThemeProvider>
-        
-    )
+      </Grid>
+
+      <Grid item xs={12}>
+        <CatedrasContenedor />
+      </Grid>
+    </Grid>
+  );
 }
