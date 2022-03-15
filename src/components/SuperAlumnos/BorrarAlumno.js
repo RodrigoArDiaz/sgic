3ebb272a2d12cae5590bdb3c55a25b8a13b7ab19
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-03-15 20:29:33 -0300

    style: Agregado de estilos a borrar alumno

--- src/components/SuperAlumnos/BorrarAlumno.js
+++ src/components/SuperAlumnos/BorrarAlumno.js
@@ -1,127 +1,104 @@
-import React from 'react';
-import { Button } from '@mui/material';
-import { Tooltip } from '@mui/material';
-import { IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useModal } from '../useModal';
+import React from "react";
+//MUI
+import { Button, Zoom } from "@mui/material";
+import { Tooltip } from "@mui/material";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+//Hooks personalizados
+import { useModal } from "../useModal";
+//React router dom
 import { useNavigate } from "react-router-dom";
+import { DeleteOutlined } from "@mui/icons-material";
 
 export const BorrarAlumno = (props) => {
-    const [isOpen, handleOpen, handleClose] = useModal(false);
-    
-    const navegar = useNavigate();
-    
+  const [isOpen, handleOpen, handleClose] = useModal(false);
 
-    
+  const navegar = useNavigate();
 
-    async function consultas(data, cadena){
+  async function consultas(data, cadena) {
+    const response = await fetch(cadena, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
 
-        const response = await fetch(cadena,
-        {
-            method: 'POST', 
-            body: JSON.stringify(data), 
-            headers:{
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            }
-          }
-        );
-    
-             return response.json();
-    }
+    return response.json();
+  }
 
+  function BorrarAlumno() {
+    var data = {
+      pidUs: props.idalumno,
+    };
 
-
-    
-    function BorrarAlumno(){
-       
-        var data = {
-            pidUs:props.idalumno,
-            
+    //console.log(props.idcatedra+"ID de la catedra");
+    consultas(data, "http://127.0.0.1:8000/api/borraralumno")
+      .then((response) => {
+        console.log(response);
+        if (response.Mensaje === "OK") {
+          handleClose();
+          props.abrir(true);
+          props.mensaje("Alumno borrado con éxito");
+          props.tipo("success");
+          props.refrescar();
+          //console.log("Borrado");
+        } else {
+          //console.log("No Borrado");
+          //setEstado('2');
+          handleClose();
+          props.abrir(true);
+          props.mensaje(response.Mensaje);
+          props.tipo("error");
         }
+      })
+      .catch((error) => {
+        console.log("Error de conexión en borrar" + error);
+        navegar("/inicio");
+      });
+  }
 
-        //console.log(props.idcatedra+"ID de la catedra");
-        consultas(data,'http://127.0.0.1:8000/api/borraralumno').then(response=>{
-            console.log(response);
-       if (response.Mensaje==='OK'){
-        
-        handleClose();
-        props.abrir(true);
-        props.mensaje('Alumno borrado con éxito');
-        props.tipo('success');
-        props.refrescar();
-        //console.log("Borrado");
-       } 
+  return (
+    <>
+      <Tooltip title="Borrar" TransitionComponent={Zoom}>
+        <span>
+          <IconButton color="secondary" size="large" onClick={handleOpen}>
+            <DeleteOutlined />
+          </IconButton>
+        </span>
+      </Tooltip>
 
-       else{
-        //console.log("No Borrado");
-        //setEstado('2');
-        handleClose();
-        props.abrir(true);
-        props.mensaje(response.Mensaje);
-        props.tipo('error');
-    
-    }
-
-         })
-        .catch(error=>{console.log("Error de conexión en borrar"+error);
-          navegar("/registrarse");
-      });  
-        }
-  
-  
-     
-  
-
-
-
-
-
-
-    
-
-
-    return (
-        <>
-            <Tooltip title="Borrar">
-                <IconButton 
-                    color='secondary'
-                    size='small'
-                    onClick={handleOpen}
-                >
-                    <DeleteIcon/>
-                </IconButton>
-            </Tooltip>
-
-            {/* Ventana modal */}
-            <Dialog 
-                open={isOpen} 
-                onClose={handleClose}
-                maxWidth="xs"
-                fullWidth
-            >
-                <DialogTitle>Borrar alumno</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                    ¿Seguro que desea borrar al alumno?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant='contained' onClick={BorrarAlumno}>Aceptar</Button>
-                    <Button variant='outlined'  color="secondary" onClick={handleClose}>Cancelar</Button>
-                </DialogActions>
-            </Dialog>  
-        </>
-    )
-    
-
-
-}
-
-
-
+      {/* Ventana modal */}
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        maxWidth="xs"
+        fullWidth
+        sx={{
+          backdropFilter: "blur(0.8px)",
+        }}
+      >
+        <DialogTitle>Borrar alumno</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ¿Seguro que desea borrar al alumno?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={BorrarAlumno}>
+            Aceptar
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
