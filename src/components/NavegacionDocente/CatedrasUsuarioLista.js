8373e5a8fa7546daf7042b2e1fd7c8f926f212ad
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-06-12 12:54:41 -0300

    feat: Cambio de titulo en modulo DocenteNavegacion

--- src/components/NavegacionDocente/CatedrasUsuarioLista.js
+++ src/components/NavegacionDocente/CatedrasUsuarioLista.js
@@ -1,8 +1,18 @@
 import * as React from "react";
 import { Grid } from "@mui/material";
 import TarjetaCatedra from "./TarjetaCatedra";
+//Redux
+import { useDispatch } from "react-redux";
+import { actualizarTitulo } from "../../store/slices/menuSlice";
 
 export default function StickyHeadTable(props) {
+  //Para el uso de funciones de los state de redux
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(actualizarTitulo("Seleccione la cátedra"));
+  }, []);
+
   if (props.filas.res === undefined) return <h4>Error fatal</h4>;
   if (props.filas.res.length < 1)
     return <h4>Consulte al administrador para asociarse a una cátedra</h4>;
--- src/components/NavegacionDocente/TarjetaCatedra.js
+++ src/components/NavegacionDocente/TarjetaCatedra.js
@@ -5,8 +5,14 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+//Redux
+import { useDispatch } from "react-redux";
+import { actualizarTitulo } from "../../store/slices/menuSlice";
 
-export default function OutlinedCard(props) {
+export default function TarjetaCatedra(props) {
+  //Para el uso de funciones de los state de redux
+  const dispatch = useDispatch();
+
   return (
     // <Box sx={{ minWidth: 100 }}>
     <Card variant="outlined">
@@ -22,7 +28,9 @@ export default function OutlinedCard(props) {
             onClick={() => {
               props.salto("2");
               props.setCat(props.idcatedra);
-              props.setT("Seleccione la materia");
+              // props.setT("Seleccione la materia");
+              //Actualiza el titulo al montar la pagina
+              dispatch(actualizarTitulo("Seleccione la materia"));
             }}
           >
             Ingresar
