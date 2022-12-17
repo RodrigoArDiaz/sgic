1b1f2bbdd62d3b5ef677ab9b35d3d25f351426f3
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2022-12-17 11:36:05 -0300

    Style y fix
    Se cambia el estilo del modulo nota de examenes. Se cambia input por input editable

--- src/components/Notas2/NotasExamenes/BotonNota.js
+++ src/components/Notas2/NotasExamenes/BotonNota.js
@@ -1,206 +1,160 @@
-import * as React from 'react';
-import {  Grid } from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import FormControl from '@mui/material/FormControl';
-import CircularProgress from '@mui/material/CircularProgress';
-import IconButton from '@mui/material/IconButton';
-import { Tooltip } from '@mui/material';
-import * as Responses from '../../Responses';
+import * as React from "react";
+import { useState } from "react";
+//MUI
+import { useTheme } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+//Responses
+import * as Responses from "../../Responses";
+//React router
 import { useNavigate } from "react-router-dom";
+//React spinner
+import { ScaleLoader } from "react-spinners";
+//Formik, yup
+import { useFormik } from "formik";
+import * as yup from "yup";
+import OutlinedInputEditableNote from "../../Material UI - Componentes Modificados/ComponentesNotas/OutlinedInputEditableNote";
 
-
-const style3 = {
-  //verde
-    width: {xs: 50},
-    
-    //color:'red',
-    backgroundColor: {xs:"#f5f5f5"},
-    //border: "solid black 2px"
-  }
-  
-
-// equivalente en verde E6FDE9  #e6fde9
-const style1 = {
-//verde
-  width: {xs: 50},
-  
-  //color:'red',
-  backgroundColor: {xs:"#fdeae6"},
-  //border: "solid black 2px"
-}
-
-
-
-const style2 = {
-// rojo
-  width: {xs: 50},
-  
-  //color:'red',
-  backgroundColor: {xs:"#e6fde9"},
-  //border: "solid black 2px"
-}
-
-var estilo;
+/*****************************************
+ * Componente 'BotonNotaGrupo'
+ */
 export const BotonNota = (props) => {
+  //Para estilos
+  const theme = useTheme();
+  const colorBgSpinner = theme.palette.secondary.main;
 
+  //Navegacion React Router
   const navegar = useNavigate();
 
-function Transformar(param){
-if (param===-5){
-return '-';
-}
-return param;
-}
+  //Variable de estado que indica la nota
+  const [nota, setNota] = useState(props.Nota < 0 ? "" : props.Nota);
 
+  //Variable de estado que indica el procesamiento de la peticion
+  const [loading, setLoading] = useState(false);
 
+  //Valores iniciales del objeto formik
+  const valoresInicialesForm = { nota: nota };
 
-  const [texto, setTexto] = React.useState(''); 
-       
-  
-  const [salto, setSalto] = React.useState('1'); 
+  //Objeto formik
+  const formik = useFormik({
+    enableReinitialize: true, //Se habilita el reinicio del form
+    initialValues: valoresInicialesForm, //Se setean valores iniciales
+    validationSchema: yup.object({}), //Se setea el esquema de validacion (vacio)
+    //Handle del evento submit
+    onSubmit: (values) => {
+      handleSubmit(values.nota);
+    },
+  });
 
-  const [nomb, setNombre] = React.useState(Transformar(props.Nota)); 
-  
-  function cambio2(e){
-setTexto(e.target.value);
-  }
-
-
-  function cambio(){
-    
-    
-   
-    if (texto!==''&& !isNaN(texto)){
-  
-      if(parseInt(texto)<0){
-        setTexto('');
- props.abrir(true);
- props.mensaje('La nota debe ser mayor que 0');
- props.tipo('error');
-      }
-      else if(parseInt(texto)>props.Escala){
-        setTexto('');
+  //Handle submit
+  const handleSubmit = (notaParam) => {
+    console.log(notaParam);
+    console.log(props.Escala);
+    //Control: nota es numero
+    if (!isNaN(notaParam)) {
+      console.log("es numero");
+      //Control: nota mayor a 0
+      if (parseInt(notaParam) < 0) {
         props.abrir(true);
-        props.mensaje('La nota debe ser menor o igual que '+props.Escala);
-        props.tipo('error');
+        props.mensaje("La nota debe ser mayor que 0");
+        props.tipo("error");
+        formik.resetForm();
       }
-
-
-      
-else{
-  var data = {
-    pNota:texto,    
-    pidAl:props.IdUsuario,
-    //pidG:'',
-     pidDoc:localStorage.getItem('tkn2'), //IdDocente aunque aqui va el token
-     pidE:props.IdExamen,
-     pidCu:props.cursada.IdCursada,                                        
-  }
-      setSalto('2');
- Responses.consultas(data,'http://127.0.0.1:8000/api/modificarnotaexamen').then(response=>{
- 
-  if(Responses.status===200){
-       
-    if(parseInt(texto)===0){
-      setNombre('-');
-      props.mensaje('Nota modificada: '+'-');
+      //Comprueba si nota esta dentro de la escala
+      else if (parseInt(notaParam) > props.Escala) {
+        props.abrir(true);
+        props.mensaje("La nota debe ser menor o igual que " + props.Escala);
+        props.tipo("error");
+        formik.resetForm();
+      }
+      //Comprueba si nuevo valor de nota es igual que el anterior
+      else if (notaParam.trim() == nota) {
+        // No se modifica si la nota es igual a la anterior
+      }
+      //Comprobacion completa, se realiza petición
+      else {
+        //Se indica que se esta realizando la peticion
+        setLoading(true);
+        //Estructura de datos a enviar
+        var data = {
+          pNota: notaParam,
+          pidAl: props.IdUsuario,
+          //pidG: props.IdGrupo,
+          pidDoc: localStorage.getItem("tkn2"), //IdDocente aunque aqui va el token
+          pidE: props.IdExamen,
+          pidCu: props.cursada.IdCursada,
+        };
+        //Peticion
+        Responses.consultas(
+          data,
+          "http://127.0.0.1:8000/api/modificarnotaexamen"
+        )
+          .then((response) => {
+            setLoading(false);
+            if (Responses.status === 200) {
+              // if (parseInt(notaParam) === 0) {
+              //   setNombre("-");
+              //   props.mensaje("Nota modificada: " + "-");
+              // } else {
+              //   setNombre(notaParam);
+              props.mensaje("Nota modificada: " + notaParam);
+              props.abrir(true);
+              props.tipo("success");
+              setNota(notaParam);
+            } else if (Responses.status === 401) {
+              navegar("/ingreso");
+            } else if (Responses.status === 460) {
+              // setTexto("");
+              // setSalto("1");
+              // if (response.nota1 !== undefined) {
+              //   props.abrir(true);
+              //   props.mensaje(response.nota1);
+              //   props.tipo("error");
+              // } else if (response.nota2 !== undefined) {
+              //   props.abrir(true);
+              //   props.mensaje(response.nota2);
+              //   props.tipo("error");
+              // }
+            } else {
+              navegar("/error");
+            }
+          })
+          .catch((error) => {
+            navegar("/error");
+          });
+      }
+    } else {
+      formik.resetForm();
     }
-      else{
-      setNombre(texto);
-      props.mensaje('Nota modificada: '+texto);
-    }
-      setTexto('');
-    
-      
-      props.abrir(true);
-      
-      props.tipo('success');
-      setSalto('1');
-     
-              }
-              else if(Responses.status===401){
-                navegar("/ingreso");
-              }
-         else if (Responses.status===460){
-          setTexto('');
-          setSalto('1');
-          if (response.nota1!==undefined){
-           props.abrir(true);
-           props.mensaje(response.nota1);
-           props.tipo('error');
-           
-              }
-             
-             
-              else if (response.nota2!==undefined){
-               props.abrir(true);
-               props.mensaje(response.nota2);
-               props.tipo('error');
-               
-                 }
-         }    
-              else {
-                navegar("/error");
-              }
- })
- .catch(error=>{console.log("Error de conexión"+error);});
- 
+  };
 
-}
-
- 
-  }
-  setTexto('');
-}
-
-
-  if (salto==='1'){
-  if(nomb==='-'){estilo=style3;
-
-  }  
-  else if(parseInt(nomb)<props.NMA){estilo=style1;  }
-    else{estilo=style2; }
-    return(
-  
+  return (
     <>
-
-     
-  <FormControl>
-    
-    <InputLabel  htmlFor="component-outlined" >{nomb}</InputLabel>
-    <OutlinedInput
-    sx={{...estilo, width: 50,
-      height: 32}}
-      id={props.var}
-     value={texto}
-      onBlur={()=>cambio()}
-      onChange={(e)=>cambio2(e)}
-      label={nomb}
-      
-    />
-
-    </FormControl>
+      {!loading ? (
+        <FormControl fullWidth>
+          <OutlinedInputEditableNote
+            id="grupo"
+            type="text"
+            placeholder="-"
+            size="small"
+            name="nota"
+            value={formik.values.nota}
+            onChange={formik.handleChange}
+            onBlur={formik.handleSubmit}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                formik.handleSubmit();
+              }
+            }}
+            notaEstilo={nota}
+            notaMinEstilo={props.NMA}
+          />
+        </FormControl>
+      ) : (
+        <IconButton aria-label="estado3" size="small" color="inherit">
+          <ScaleLoader color={colorBgSpinner} width="0.5" height="15" />
+        </IconButton>
+      )}
     </>
-    );
-    
-    
-    
-    }
-    
-if (salto==='2'){
-  
-
-    return(
-<Grid item xs={12} sm="auto">
-<Tooltip title="Verificando">
-<IconButton aria-label="estado3"  size='small' color="inherit"  >
-        <CircularProgress />
-      </IconButton>
-      </Tooltip>
-      </Grid>
-        );
-    }
-
-}
-
+  );
+};
--- src/components/Notas2/NotasExamenes/NotasExamenesLista.js
+++ src/components/Notas2/NotasExamenes/NotasExamenesLista.js
@@ -14,6 +14,13 @@ import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { Grid } from "@mui/material";
 import FilasPorPagina from "../../GestionCatedrasSuper/FilasPorPagina";
+import PaginationCustom from "../../Material UI - Componentes Modificados/ComponentePaginacion/PaginationCustom";
+import ContenedorFilasPorPagina from "../../Material UI - Componentes Modificados/ComponentePaginacion/ContenedorFilasPorPagina";
+import ContenedorResultados from "../../Material UI - Componentes Modificados/ComponentePaginacion/ContenedorResultados";
+import {
+  TableCell1em,
+  TableRowElevacion,
+} from "../../Material UI - Componentes Modificados/ComponentesTabla";
 
 export default function StickyHeadTable(props) {
   var columns = [
@@ -67,139 +74,130 @@ export default function StickyHeadTable(props) {
   if (props.filas.res.length < 1) return <h4>No se encontraron resultados</h4>;
 
   return (
-    <ThemeProvider theme={theme}>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: "none" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => {
-                  if (
-                    column.id === "Apellidos" ||
-                    column.id === "Nombres" ||
-                    column.id === "Libreta"
-                  ) {
-                    return (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    );
-                  } else {
-                    return (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ width: column.minWidth }}
-                      >
-                        <ECContenedorExamenes
-                          nombre={column.label}
-                          IdExamen={column.idp}
-                          cursada={props.cursada}
-                          Nombre={column.label}
-                        />
-                      </TableCell>
-                    );
-                  }
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {props.filas.res.map((row) => {
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={row.IdUsuario}
-                  >
-                    {columns.map((column) => {
-                      const value = row[column.id];
-
-                      if (column.id === "Apellidos") {
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      } else if (column.id === "Nombres") {
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      } else if (column.id === "Libreta") {
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      } else {
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number" ? (
-                              column.format(value)
-                            ) : (
-                              <BotonNota
-                                cursada={props.cursada}
-                                Nota={value}
-                                IdUsuario={row.IdUsuario}
-                                IdExamen={column.idp}
-                                abrir={props.abrir}
-                                mensaje={props.mensaje}
-                                tipo={props.tipo}
-                                NMA={column.NMA}
-                                Escala={column.Escala}
-                              />
-                            )}
-                          </TableCell>
-                        );
-                      }
-                    })}
-                  </TableRow>
-                );
+    <>
+      <TableContainer sx={{ maxHeight: "none" }}>
+        <Table aria-label="Lista de finales" sx={{ mb: "1rem" }} size="small">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => {
+                if (
+                  column.id === "Apellidos" ||
+                  column.id === "Nombres" ||
+                  column.id === "Libreta"
+                ) {
+                  return (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  );
+                } else {
+                  return (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ width: column.minWidth }}
+                    >
+                      <ECContenedorExamenes
+                        nombre={column.label}
+                        IdExamen={column.idp}
+                        cursada={props.cursada}
+                        Nombre={column.label}
+                      />
+                    </TableCell>
+                  );
+                }
               })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Grid justifyContent="flex-start" container pt={2}>
-          <Grid item xs={6} sx={{ mt: 1 }}>
-            <Stack spacing={2}>
-              <Pagination
-                variant="outlined"
-                defaultPage={1}
-                count={props.paginacion}
-                page={props.pagina}
-                onChange={(e, page) => CambiarPagina(e, page)}
-              />
-            </Stack>
-          </Grid>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.filas.res.map((row) => {
+              return (
+                <TableRowElevacion key={row.IdUsuario}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
 
-          <Grid item xs={3} sx={{ mt: 1 }}>
-            Filas por página:{" "}
-            {
-              <FilasPorPagina
-                actualizarfilas={props.actualizarfilas}
-                fpp={props.filasxpagina}
-              />
-            }
-          </Grid>
-
-          <Grid item xs={3} sx={{ mt: 1 }}>
-            Resultados: {props.resultados}
-          </Grid>
+                    if (column.id === "Apellidos") {
+                      return (
+                        <TableCell1em key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell1em>
+                      );
+                    } else if (column.id === "Nombres") {
+                      return (
+                        <TableCell1em key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell1em>
+                      );
+                    } else if (column.id === "Libreta") {
+                      return (
+                        <TableCell1em key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell1em>
+                      );
+                    } else {
+                      return (
+                        <TableCell1em key={column.id} align={column.align}>
+                          {column.format && typeof value === "number" ? (
+                            column.format(value)
+                          ) : (
+                            <BotonNota
+                              cursada={props.cursada}
+                              Nota={value}
+                              IdUsuario={row.IdUsuario}
+                              IdExamen={column.idp}
+                              abrir={props.abrir}
+                              mensaje={props.mensaje}
+                              tipo={props.tipo}
+                              NMA={column.NMA}
+                              Escala={column.Escala}
+                            />
+                          )}
+                        </TableCell1em>
+                      );
+                    }
+                  })}
+                </TableRowElevacion>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Grid justifyContent="flex-start" container pt={2} paddingRight={2}>
+        <Grid item xs={8} sx={{ mt: 1 }}>
+          <Stack spacing={2}>
+            <PaginationCustom
+              defaultPage={1}
+              count={props.paginacion}
+              page={props.pagina}
+              onChange={(e, page) => CambiarPagina(e, page)}
+            />
+          </Stack>
         </Grid>
-        <Grid justifyContent="center" container pt={2} />
-      </Paper>
-    </ThemeProvider>
+
+        <ContenedorFilasPorPagina>
+          Filas por página:{" "}
+          {
+            <FilasPorPagina
+              actualizarfilas={props.actualizarfilas}
+              fpp={props.filasxpagina}
+            />
+          }
+        </ContenedorFilasPorPagina>
+
+        <ContenedorResultados>
+          Resultados: {props.resultados}
+        </ContenedorResultados>
+      </Grid>
+    </>
   );
 }
