4a4bd1e25c191e413ff503b6e6f64ebe75239f8a
Diaz Rodrigo Armando <diazrodrigoredes@gmail.com>
2023-01-22 00:43:38 -0300

    fix: breadcumbs

--- src/components/BreadCrumbs.js
+++ src/components/BreadCrumbs.js
@@ -1,82 +1,44 @@
 import * as React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 // import { Box } from "@mui/material";
 import { Box } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
+import { Link as RouterLink } from "react-router-dom";
 
 function handleClick(event) {
   event.preventDefault();
   console.info("You clicked a breadcrumb.");
 }
 
-export default function BreadCrumbs() {
-  // const breadcrumbs = [
-  //   <Link
-  //     underline="hover"
-  //     key="1"
-  //     color="inherit"
-  //     href="/"
-  //     onClick={handleClick}>
-  //     SGIC
-  //   </Link>,
-  //   <Typography key="3" color="text.primary">
-  //     Catedras
-  //   </Typography>,
-  // ];
-
-  const crumbs = [
-    {
-      nombreRuta: "Inicio",
-      href: "enlace",
-    },
-    {
-      nombreRuta: "Inicio",
-      href: "enlace",
-    },
-    {
-      nombreRuta: "Inicio",
-      href: "enlace",
-    },
-  ];
+export default function BreadCrumbs({ crumbs }) {
+  const last = crumbs ? crumbs.length - 1 : false;
 
   return (
-    <Box py={1} pl={2}>
+    <Box>
       <Stack spacing={2}>
-        <Breadcrumbs
-          separator={<NavigateNextIcon fontSize="small" />}
-          aria-label="breadcrumb"
-        >
-          {/* {breadcrumbs} */}
-          <Link
-            underline="hover"
-            key="1"
-            sx={{ display: "flex", alignItems: "center" }}
-            color="inherit"
-            href="/"
-            onClick={handleClick}
+        {crumbs && (
+          <Breadcrumbs
+            // separator={<NavigateNextIcon fontSize="small" />}
+            aria-label="breadcrumb"
           >
-            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-            Inicio
-          </Link>
-
-          {crumbs.map((crumb, index) => {
-            return (
-              <Link
-                underline="hover"
-                key="1"
-                color="inherit"
-                href="/"
-                onClick={handleClick}
-              >
-                SGIC
-              </Link>
-            );
-          })}
-        </Breadcrumbs>
+            {crumbs.map((crumb, index) => {
+              return (
+                <>
+                  <Link
+                    underline="hover"
+                    color={last == index ? "text.primary" : "inherit"}
+                    to={crumb.to}
+                    component={RouterLink}
+                  >
+                    {crumb.nombreRuta}
+                  </Link>
+                </>
+              );
+            })}
+          </Breadcrumbs>
+        )}
       </Stack>
     </Box>
   );
