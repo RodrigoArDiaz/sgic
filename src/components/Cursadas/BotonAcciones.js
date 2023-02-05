118851533e18f3bc0d0ff3f41b50e4e95b7786c0
DylanSD0 <dylansdiaz.utn@gmail.com>
2023-02-05 00:37:22 -0300

    re-code: cambio de estructura de agregar parametro

--- src/components/Cursadas/BotonAcciones.js
+++ src/components/Cursadas/BotonAcciones.js
@@ -5,10 +5,24 @@ import { ModificarCursada } from "./ModificarCursada";
 import { AgregarParametro } from "./AgregarParametro";
 import { ListarParametros } from "./ListarParametros/ListarParametros";
 import { ClonarCursada } from "./Opciones/ClonarCursada";
+import { AgregarParametroPrueba } from "./AgregarParametroPrueba";
 
 export const BotonAcciones = (props) => {
   return (
     <Grid container justifyContent="space-between" flexWrap="nowrap">
+      {/* Prueba */}
+      {/* <Grid item xs={12} sm="auto">
+        <AgregarParametroPrueba
+          anio={props.anio}
+          idcursada={props.idcursada}
+          semestre={props.semestre}
+          Materia={props.Materia}
+          abrir={props.abrir}
+          mensaje={props.mensaje}
+          tipo={props.tipo}
+        />
+      </Grid> */}
+
       <Grid item xs={12} sm="auto">
         <AgregarParametro
           anio={props.anio}
